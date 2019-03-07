54e2fd12f136470c9732a7c0e2de797f7fcc02b7
Anthony <a.chiho13@gmail.com>
2019-03-07 00:09:07 +0000

    fixed small bug dragging

--- frontend/components/Location.js
+++ frontend/components/Location.js
@@ -8,8 +8,8 @@ import Draggable, {DraggableCore} from 'react-draggable';
 import Router from 'next/router';
 
 const snappedPositions = {
-  closed: 0,
-  orignalHeight: 180
+    closed: 0,
+    orignalHeight: 180
 };
 
 export default class Location extends Component {
@@ -18,7 +18,7 @@ export default class Location extends Component {
     };
 
     state = {
-        height:  snappedPositions.orignalHeight,
+        height: snappedPositions.orignalHeight,
         touchStartPos: 0,
         draggable: false,
         expanded: false,
@@ -28,63 +28,85 @@ export default class Location extends Component {
     closeLocation = () => {
         this.closeLocationDetail()
     }
- 
-    dragStart = (e) => {
-      if(!this.state.draggable) return;
-      this.setState({touchStartPos: e.currentTarget.clientHeight})
+
+    dragStart = () => {
+        if (!this.state.draggable) 
+            return;
+        this.setState({touchStartPos: this.state.height})
     }
 
     calcHeight = (height) => {
-      this.setState({
-        height
-      })
+        this.setState({height})
     }
 
     expandLocation = (e, {deltaY}) => {
-      if(!this.state.draggable) return;
-      const height = this.state.height - deltaY;
-      this.calcHeight(height)
+        if (!this.state.draggable) 
+            return;
+        const height = this.state.height - deltaY;
+        this.calcHeight(height)
     }
 
     dragEnd = () => {
-      if(!this.state.draggable) return;
-      const dragLength = this.state.height - this.state.touchStartPos
-      const {closeLocation} = this.props;
-      
-      if(dragLength > 40) {
-        this.setState({height: this.state.openedHeight, expanded: true}); 
-      }  else if (dragLength < -40) {
-        if(this.state.height > snappedPositions.orignalHeight) {
-          this.setState({height: snappedPositions.orignalHeight, expanded: false});
+        if (!this.state.draggable) 
+            return;
+        const dragLength = this.state.height - this.state.touchStartPos
+        const {closeLocation} = this.props;
+
+        if (dragLength > 40) {
+            this.setState({height: this.state.openedHeight, expanded: true});
+        } else if (dragLength < -40) {
+            this.setState({expanded: false});
+            if (this.state.height > snappedPositions.orignalHeight) {
+                this.setState({height: snappedPositions.orignalHeight});
+            } else {
+                closeLocation();
+                this.setState({height: snappedPositions.closed});
+                Router.push('/');
+            }
         } else {
-          closeLocation();
-          this.setState({height: snappedPositions.closed});
-          Router.push('/');
+            this.calcHeight(this.state.touchStartPos)
         }
-      } else {
-        this.calcHeight(this.state.touchStartPos)
-      }
     }
 
     setHeight = () => {
-      const locationHeight = document.querySelector('.locationItem').clientHeight
-      this.setState({height: locationHeight});
+        const locationHeight = document
+            .querySelector('.locationItem')
+            .clientHeight
+        this.setState({height: locationHeight});
+    }
+
+    onOrientationChange() {
+      const SELF = this;
+      window.onorientationchange =  this.state.expanded && function () {
+        setTimeout(function () {
+            SELF.calcHeight(window.innerHeight);
+            SELF.setState({openedHeight: window.innerHeight});
+        }, 200)
+    }
     }
 
     componentDidMount() {
-      this.setHeight()
-      window.innerWidth < 1000 && this.setState({draggable: true});
+        this.setHeight()
+        window.innerWidth < 1000 && this.setState({draggable: true});
+        console.log(window.innerWidth);
     }
 
     render() {
         const {location, closeLocation, isOpened} = this.props;
-
+         this.onOrientationChange();
         return (
-            <DraggableCore axis="y" onStart={this.dragStart} onStop={this.dragEnd} onDrag={this.expandLocation}>
+            <DraggableCore
+                axis="y"
+                onStart={this.dragStart}
+                onStop={this.dragEnd}
+                onDrag={this.expandLocation}>
                 <LocationItemStyles
                     isExpanded={this.state.expanded}
                     isOpened={isOpened}
-                    className="locationItem" style={{height: this.state.height}}>
+                    className="locationItem"
+                    style={{
+                    height: this.state.height
+                }}>
                     <div className="dragNib"></div>
                     <Link href={{
                         pathname: '/'
