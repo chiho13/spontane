076ad7677aa80231fcdc1b72f92fd2749c61231b
Anthony <a.chiho13@gmail.com>
2019-05-16 22:02:37 +0100

    refactor auth signup

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,8 +1,6 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const Auth = require('./Auth');
 
 const Mutations = {
- 
     async createGeoLocation(parent, args, ctx, info) {
 
         const geolocation = await ctx.db.mutation.createGeoLocation({
@@ -44,40 +42,18 @@ const Mutations = {
         );
       },
 
-      async deleteLocation(parent, args, ctx, info) {
+    async deleteLocation(parent, args, ctx, info) {
           const where = {id: args.id}
 
         //   const location = await ctx.db.query.location({where}, `id title`);
 
           return ctx.db.mutation.deleteLocation({ where }, info);
-      },
+    },
 
-      async signup(parent, args, ctx, info) {
-        // lowercase their email
-        args.email = args.email.toLowerCase();
-        // hash their password
-        const password = await bcrypt.hash(args.password, 10);
-        // create the user in the database
-        const user = await ctx.db.mutation.createUser(
-          {
-            data: {
-              ...args,
-              password,
-              permissions: { set: ['USER'] },
-            },
-          },
-          info
-        );
-        // create the JWT token for them
-        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-        // We set the jwt as a cookie on the response
-        ctx.response.cookie('token', token, {
-          httpOnly: true,
-          maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-        });
-        // Finalllllly we return the user to the browser
-        return user;
-      },
+    signup(parent, args, ctx, info) {
+        const user = Auth.signup(parent, args, ctx, info);
+        return user
+    }
 };
 
 module.exports = Mutations;
