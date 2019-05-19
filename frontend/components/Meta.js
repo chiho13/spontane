3445c4677d12865d72a8b1ada30c6a7ea39b65e0
Anthony <a.chiho13@gmail.com>
2019-05-19 15:57:55 +0100

    sign in works

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -5,8 +5,6 @@ const Meta = () => (
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=no"/>
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/static/favicon.png" />
-        <script src='/static/nprogress.js'></script>
-        <link rel="stylesheet" type="text.css" href="/static/nprogress.css" />
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet" />
--- frontend/components/Signup/Signup.js
+++ frontend/components/Signup/Signup.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {Mutation} from 'react-apollo';
 import gql from 'graphql-tag';
 import Error from '../ErrorMessage';
@@ -10,7 +10,17 @@ import Link from 'next/link';
 
 const invertTheme = ({white, brandColor}) => ({black: white, white: brandColor, hoverColor: '#006fe6'});
 
-const SignupStyles = styled.div`
+const SIGNUP_MUTATION = gql `
+    mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+        signup(email: $email, name: $name, password: $password) {
+            id
+            email
+            name
+        }
+    }
+`;
+
+const SignupStyles = styled.div `
     position: relative;
     top: 100px;
 
@@ -35,53 +45,73 @@ const SignupStyles = styled.div`
 
 function Signup() {
     const [form,
-        handleChange] = useForm({email: '', name: '', password: ''});
+        setForm] = useState({email: '', name: '', password: ''});
 
+    function handleChange(e) {
+        const {name, value} = e.target;
+        setForm({...form, [name]: value});
+    }
     return (
-    <SignupStyles>
-        <div className="loginLink">
-            Already have an account? <Link href="/login"><a>Log in</a></Link>
-        </div>
-        <Form width="360px" top="0" right="0">
-            <fieldset>
-                <h2>Sign up for an account</h2>
-                <div></div>
-                <label htmlFor="email">
-                    Email<sup className="required" title="required">*</sup>
-                </label>
-                <input
-                    id="email"
-                    type="text"
-                    name="email"
-                    placeholder="email"
-                    value={form.email}
-                    onChange={handleChange} required/>
+        <SignupStyles>
+            <div className="loginLink">
+                Already have an account?
+                <Link href="/login">
+                    <a>Log in</a>
+                </Link>
+            </div>
 
-                <label htmlFor="name">
-                    Name<sup className="required" title="required">*</sup></label>
-                <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    placeholder="name"
-                    value={form.name}
-                    onChange={handleChange} required/>
+            <Mutation mutation={SIGNUP_MUTATION} variables={form}>
+                {(signup, {error, loading}) => {
+                    return <Form width="360px" top="0" right="0" method="post" onSubmit={e => {
+                        e.preventDefault();
+                        signup();
+                    }}>
+                        <fieldset disabled={loading}>
+                            <h2>Sign up for an account</h2>
+                            <Error error={error}/>
+                            <label htmlFor="email">
+                                Email<sup className="required" title="required">*</sup>
+                            </label>
+                            <input
+                                id="email"
+                                type="text"
+                                name="email"
+                                placeholder="email"
+                                value={form.email}
+                                onChange={handleChange}
+                                required/>
 
-                <label htmlFor="password">
-                    Password<sup className="required" title="required">*</sup>
-                </label>
-                <input
-                    id="password"
-                    type="text"
-                    name="password"
-                    placeholder="password"
-                    value={form.password}
-                    onChange={handleChange} required/>
-                <ThemeProvider theme={invertTheme}>
-                    <Button type="submit" disableRipple>Sign Up</Button>
-                </ThemeProvider>
-            </fieldset>
-        </Form>
+                            <label htmlFor="name">
+                                Name<sup className="required" title="required">*</sup>
+                            </label>
+                            <input
+                                id="name"
+                                type="text"
+                                name="name"
+                                placeholder="name"
+                                value={form.name}
+                                onChange={handleChange}
+                                required/>
+
+                            <label htmlFor="password">
+                                Password<sup className="required" title="required">*</sup>
+                            </label>
+                            <input
+                                id="password"
+                                type="text"
+                                name="password"
+                                placeholder="password"
+                                value={form.password}
+                                onChange={handleChange}
+                                required/>
+                            <ThemeProvider theme={invertTheme}>
+                                <Button type="submit" disableRipple>Sign Up</Button>
+                            </ThemeProvider>
+                        </fieldset>
+                    </Form>
+                }}
+            </Mutation>
+
         </SignupStyles>
     )
 }
