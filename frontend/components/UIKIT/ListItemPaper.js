55d52d1883444165b1682b9c957390372df032cd
Anthony <a.chiho13@gmail.com>
2019-05-24 18:51:28 +0100

    padding list

--- frontend/components/UIKIT/ListItemPaper.js
+++ frontend/components/UIKIT/ListItemPaper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ListItemPaper = styled.div`
     border-radius: 4px;
     box-shadow: 0 1px 5px 1px rgba(100, 105, 135, .3);
-    padding: 8px;
+    padding: 16px;
     max-width: 100%;
     margin-top: 16px;
 
