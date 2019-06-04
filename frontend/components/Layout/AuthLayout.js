eeeeb316b32687b0ffe81040d2cdb28e713ad135
Anthony <a.chiho13@gmail.com>
2019-06-04 19:57:40 +0100

    theme logo

--- frontend/components/Layout/AuthLayout.js
+++ frontend/components/Layout/AuthLayout.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Logo from '../Logo';
 
 const AuthStyle = styled.div`
 display: block;
@@ -7,10 +8,20 @@ position: fixed;
 width: 100%;
 background: linear-gradient(45deg, rgba(0,123,255,1) 0%, rgba(66,151,255,1) 100%);
 overflow-y: auto;
+
+svg {
+  position: absolute;
+  top: 3rem;
+  left: 4rem;
+  width: 180px;
+}
 `;
+
+const whitelogo = ({white}) => ({brandColor: white});
 
 const AuthLayout = props => (
   <AuthStyle>
+    <Logo theme={whitelogo} />
     {props.children}
   </AuthStyle>
 );
