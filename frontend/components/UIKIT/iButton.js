df2b03c8f526bd7b1a94957792659a02d5813b6c
Anthony <a.chiho13@gmail.com>
2020-04-28 14:42:22 +0100

    update packages

--- frontend/components/UIKIT/iButton.js
+++ frontend/components/UIKIT/iButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/Button';
 
 const iButton = styled(Button)`
 
