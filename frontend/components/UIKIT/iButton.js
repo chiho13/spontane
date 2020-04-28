a2892d980cd27fd900555e9fe3bec58dd3056137
Anthony <a.chiho13@gmail.com>
2020-04-28 15:44:39 +0100

    Revert "update packages"
    This reverts commit df2b03c8f526bd7b1a94957792659a02d5813b6c.

--- frontend/components/UIKIT/iButton.js
+++ frontend/components/UIKIT/iButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from '@material-ui/Button';
+import Button from '@material-ui/core/Button';
 
 const iButton = styled(Button)`
 
